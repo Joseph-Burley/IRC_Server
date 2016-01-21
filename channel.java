7afe2cf1839d641624a0f007440e9ab62e072a53
Joseph Burley <jburley3@students.kennesaw.edu>
2016-01-21 16:13:41 -0500

    adding a skeleton for channels

---
+++ channel.java
@@ -0,0 +1,12 @@
+//this is the channel class
+//this will communicate with user objects
+//and will log channel communications
+import java.util.*; //lists
+
+public class channel{
+	private List<users> userList;
+	private Socket sock;
+	
+	String name;
+	
+}
