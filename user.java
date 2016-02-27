871a6a2e8e4fa35bc0a50678351dce8b8a4766a3
Joseph Burley <jburley3@students.kennesaw.edu>
2016-02-27 17:26:26 -0500

    general work

--- user.java
+++ user.java
@@ -10,6 +10,8 @@ public class user extends Thread implements globals
    private String sentence;
    private String userQuit = "/quit";
    private String changeNick = "/nick";
+   private String addCommand = "/add";
+   private String joinCommand = "/join";
    private Socket connection;
    private List<channel> localChannels = new ArrayList<channel>();
    private channel activeChannel;
@@ -38,14 +40,14 @@ public class user extends Thread implements globals
       return nickName;
    }
    
-   private void changeChannel(String chName)
+   private void addChannel(String chName)
    {
       boolean changed = false;
-      for(int i=0; i< globalChannels.size(); i++)
+      for(int i=0; i< localChannels.size(); i++)
       {
-         if(globalChannels.get(i).getName().equals(chName))
+         if(localChannels.get(i).getName().equals(chName))
          {
-            activeChannel = globalChannels.get(i);
+            activeChannel = localChannels.get(i);
             write("Changed to channel: " + activeChannel.getName());
             changed = true;
             break;
@@ -53,6 +55,20 @@ public class user extends Thread implements globals
       }
       if(!changed)
          write("Channel " + chName + " does not exist.");
+      else
+         write("Switched to " + chName);
+   }
+   
+   public void joinChannel(String chName)
+   {
+      for(int i=0; i<globalChannels.size(); i++)
+      {
+         if(globalChannels.get(i).getName().equals(chName));
+         {
+            localChannels.add(globalChannels.get(i));
+            break;
+         }
+      }
    }
    
    public void run() 
@@ -97,6 +113,22 @@ public class user extends Thread implements globals
                System.out.println("Cannot send message to client\n" + e);
             }
             
+         }
+         else if(sentence.contains(addCommand))
+         {
+            int space_index = sentence.indexOf(" ");
+            if(space_index > 0)
+            {
+               addChannel(sentence.substring(space_index, sentence.length()));
+            }
+         }
+         else if(sentence.contains(joinCommand))
+         {
+            int space_index = sentence.indexOf(" ");
+            if(space_index > 0)
+            {
+               joinChannel(sentence.substring(space_index, sentence.length()));
+            }
          }
          else if(sentence.length() > 0)
          {   
